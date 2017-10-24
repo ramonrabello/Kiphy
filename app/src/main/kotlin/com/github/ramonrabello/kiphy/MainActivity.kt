a858701401092c53084bbe1e0e57aeadb7db538f
Ramon Rabello <ramon.rabello@colab.re>
2017-10-24 03:03:35 -0200

    refactoring to show error dialog and messages.

--- app/src/main/kotlin/com/github/ramonrabello/kiphy/MainActivity.kt
+++ app/src/main/kotlin/com/github/ramonrabello/kiphy/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.github.ramonrabello.kiphy
 
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.StaggeredGridLayoutManager
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.progress_bar
-import kotlinx.android.synthetic.main.activity_main.trends_recycler_view
 import com.github.ramonrabello.kiphy.data.Trending
 import com.github.ramonrabello.kiphy.data.gone
 import com.github.ramonrabello.kiphy.data.visible
@@ -14,13 +15,14 @@ import com.github.ramonrabello.kiphy.presentation.trends.SpacingItemDecoration
 import com.github.ramonrabello.kiphy.presentation.trends.TrendingAdapter
 import com.github.ramonrabello.kiphy.presentation.trends.TrendingContract
 import com.github.ramonrabello.kiphy.presentation.trends.TrendingPresenter
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Main Activity for the app.
  */
 class MainActivity : AppCompatActivity(), TrendingContract.View {
 
-    lateinit var trendingPresenter:TrendingContract.Presenter
+    private lateinit var trendingPresenter:TrendingContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,16 +44,25 @@ class MainActivity : AppCompatActivity(), TrendingContract.View {
         trends_recycler_view.visible()
     }
 
-    override fun showTrending(trending: Trending?) {
+    override fun showTrending(trending: Trending) {
 
         trends_recycler_view.apply {
             layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
             addItemDecoration(SpacingItemDecoration(10))
-            adapter = TrendingAdapter(trending!!.data)
+            adapter = TrendingAdapter(trending.data)
         }
     }
 
-    override fun onLoadingTrendsError() {
-        Toast.makeText(this, "Oh oh! Error loading trends :(", Toast.LENGTH_SHORT).show()
+    override fun showTrendingError() {
+        Snackbar.make(coordinator_layout, getString(R.string.trending_error), Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun showApikeyError() {
+        val alertDialogError = AlertDialog.Builder(this)
+                .setTitle(getString(R.string.dialog_title))
+                .setPositiveButton(getString(R.string.dialog_close_button), null)
+                .setMessage(getString(R.string.api_key_error))
+                .create()
+        alertDialogError.show()
     }
 }
