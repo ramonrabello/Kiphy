6674992a90b167b911d5ad7245c5a035267106af
Ramon Rabello <ramon.rabello@colab.re>
2017-10-24 03:02:23 -0200

    removing @Headers annotation in order to use dynamic header include during request interception.

--- app/src/main/kotlin/com/github/ramonrabello/kiphy/presentation/trends/TrendingEndpoint.kt
+++ app/src/main/kotlin/com/github/ramonrabello/kiphy/presentation/trends/TrendingEndpoint.kt
@@ -3,14 +3,12 @@ package com.github.ramonrabello.kiphy.presentation.trends
 import com.github.ramonrabello.kiphy.data.Trending
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
 
 /**
  * Retrofit interface for Trending API endpoint.
  */
 interface TrendingEndpoint {
 
-    @Headers("api_key:{your_api_key_here}")
     @GET("/v1/gifs/trending")
     fun load(): Call<Trending>
 }
