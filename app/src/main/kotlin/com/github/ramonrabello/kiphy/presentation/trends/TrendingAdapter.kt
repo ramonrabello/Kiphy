2bda1ed3ec8823aa556ec46b7861b9f275e4cad3
arctouch-ramonrabello <ramon.rabello@quizlet.com>
2019-03-24 23:32:54 -0300

    Refactor to use findViewById() instead of Kotlin Extensions synthetic properties.

--- app/src/main/kotlin/com/github/ramonrabello/kiphy/presentation/trends/TrendingAdapter.kt
+++ app/src/main/kotlin/com/github/ramonrabello/kiphy/presentation/trends/TrendingAdapter.kt
@@ -2,27 +2,20 @@ package com.github.ramonrabello.kiphy.presentation.trends
 
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
-import android.support.v4.content.ContextCompat
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.TransitionOptions
-import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.load.resource.gif.GifDrawable
-import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
-import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import com.github.ramonrabello.kiphy.R
 import com.github.ramonrabello.kiphy.data.Data
 import com.github.ramonrabello.kiphy.data.tagfy
-import kotlinx.android.synthetic.main.trending_view_holder.view.*
 import java.util.*
 
 /**
@@ -35,8 +28,8 @@ class TrendingAdapter(private val data: List<Data>) : RecyclerView.Adapter<Trend
         holder.bind(data)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): TrendingViewHolder {
-        val view = LayoutInflater.from(parent?.context).inflate(R.layout.trending_view_holder, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrendingViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.trending_view_holder, parent, false)
         return TrendingViewHolder(view)
     }
 
@@ -46,6 +39,9 @@ class TrendingAdapter(private val data: List<Data>) : RecyclerView.Adapter<Trend
      * View holder for a trending item.
      */
     class TrendingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val gifImage by lazy { itemView.findViewById<ImageView>(R.id.gif_image) }
+        private val gitSlug by lazy { itemView.findViewById<TextView>(R.id.gif_slug) }
 
         /**
          * Binds data to view holder views.
@@ -68,16 +64,16 @@ class TrendingAdapter(private val data: List<Data>) : RecyclerView.Adapter<Trend
 
             // choose random color to be the image placeholder
             val randomIndex = Random().nextInt(placeholderColors.size + 1 - 1) + 1
-            Glide.with(itemView?.context)
+            Glide.with(itemView.context)
                     .asGif()
                     .load(data.images.fixed_height.url)
                     .apply(RequestOptions()
                             .diskCacheStrategy(DiskCacheStrategy.ALL)
-                            .placeholder(ColorDrawable(ContextCompat.getColor(itemView.context, placeholderColors[randomIndex-1])))
+                            .placeholder(ColorDrawable(ContextCompat.getColor(itemView.context, placeholderColors[randomIndex - 1])))
                             .centerCrop())
                     .transition(DrawableTransitionOptions.withCrossFade(android.R.anim.fade_in, 300))
-                    .into(itemView.gif_image)
-            itemView.gif_slug.text = data.slug.tagfy()
+                    .into(gifImage)
+            gitSlug.text = data.slug.tagfy()
         }
     }
 }
