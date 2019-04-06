553de58e1f29f685f402ad382253ba34cc096d91
arctouch-ramonrabello <ramon.rabello@quizlet.com>
2019-04-06 00:35:15 -0300

    Refactor NetworkError to receive Throwable parameter.

--- app/src/main/kotlin/com/github/ramonrabello/kiphy/common/arch/state/Result.kt
+++ app/src/main/kotlin/com/github/ramonrabello/kiphy/common/arch/state/Result.kt
@@ -7,7 +7,7 @@ package com.github.ramonrabello.kiphy.common.arch.state
 sealed class Result<out T : Any> {
     class Success<out T : Any>(val value: T) : Result<T>()
     sealed class Error : Result<Nothing>() {
-        object NetworkError : Error()
+        class NetworkError(error: Throwable) : Error()
         object TimeoutError : Error()
         object ApiResponseError : Error()
         object ApiKeyNotSetError : Error()
