1dffe1ba2b3d1c7527d294e177337a3e6202203c
arctouch-ramonrabello <ramon.rabello@quizlet.com>
2019-04-06 00:31:56 -0300

    Update view model to use RxJava-based repository and SingleLiveEvent extension functions.

--- app/src/main/kotlin/com/github/ramonrabello/kiphy/trends/TrendingViewModel.kt
+++ app/src/main/kotlin/com/github/ramonrabello/kiphy/trends/TrendingViewModel.kt
@@ -5,49 +5,70 @@ import com.github.ramonrabello.kiphy.BuildConfig
 import com.github.ramonrabello.kiphy.common.arch.state.Result
 import com.github.ramonrabello.kiphy.common.arch.state.SingleLiveEvent
 import com.github.ramonrabello.kiphy.common.arch.state.UiState
-import com.github.ramonrabello.kiphy.data.GiphyApi
+import com.github.ramonrabello.kiphy.common.extensions.postEmptyState
+import com.github.ramonrabello.kiphy.common.extensions.postErrorState
+import com.github.ramonrabello.kiphy.common.extensions.postLoadedState
+import com.github.ramonrabello.kiphy.common.extensions.postLoadingState
+import com.github.ramonrabello.kiphy.common.data.ApiProvider
+import com.github.ramonrabello.kiphy.trends.data.TrendingDataSource
+import com.github.ramonrabello.kiphy.trends.data.TrendingRepository
 import com.github.ramonrabello.kiphy.trends.model.TrendingResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class TrendingViewModel : ViewModel() {
 
-    private val _trendsDataEvent = SingleLiveEvent<Result<TrendingResponse>>()
+    // TODO: move to a BaseViewModel
+    private val _trendsDataEvent by lazy { SingleLiveEvent<Result<TrendingResponse>>() }
     val trendsDataEvent: SingleLiveEvent<Result<TrendingResponse>>
         get() = _trendsDataEvent
 
-    private val _uiStateEvent = SingleLiveEvent<UiState>()
+    // TODO: move to a BaseViewModel
+    private val _uiStateEvent by lazy { SingleLiveEvent<UiState>() }
     val uiStateEvent: SingleLiveEvent<UiState>
         get() = _uiStateEvent
 
-    fun loadTrends() {
-        _uiStateEvent.postValue(UiState.Loading)
-        if (BuildConfig.GIPHY_API_KEY.isEmpty()) {
-            _trendsDataEvent.postValue(Result.Error.ApiKeyNotSetError)
-        }
-
-        // TODO: extract to a repository
-        GiphyApi.trending().load().enqueue(object : Callback<TrendingResponse> {
-            override fun onResponse(call: Call<TrendingResponse>, response: Response<TrendingResponse>) {
-                if (response.isSuccessful) {
-                    response.body()?.let { trending ->
-                        _uiStateEvent.postValue(UiState.Loaded)
-                        _trendsDataEvent.postValue(Result.Success(trending))
-                    }
-                } else {
-                    handleUiError()
-                }
-            }
-
-            override fun onFailure(call: Call<TrendingResponse>, t: Throwable) {
-                handleUiError()
-            }
-        })
+    private val repository by lazy {
+        val api = ApiProvider.providesTrendingApi()
+        TrendingRepository(TrendingDataSource.Remote(api))
     }
 
-    private fun handleUiError() {
-        _uiStateEvent.postValue(UiState.Error)
-        _trendsDataEvent.postValue(Result.Error.FeatureError.TrendingError)
+    // TODO: move to a BaseViewModel
+    private val compositeDisposable by lazy { CompositeDisposable() }
+
+    fun loadTrending() {
+        if (BuildConfig.GIPHY_API_KEY.isEmpty()) {
+            _uiStateEvent.postErrorState()
+            _trendsDataEvent.postValue(Result.Error.ApiKeyNotSetError)
+        } else {
+            val disposable = repository.loadTrends()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe { _uiStateEvent.postLoadingState() }
+                    .doOnSuccess { trending -> onTrendingLoaded(trending) }
+                    .doOnError { error -> onError(error) }
+                    .subscribe()
+            compositeDisposable.add(disposable)
+        }
+    }
+
+    private fun onTrendingLoaded(trending: TrendingResponse) {
+        if (trending.data.isEmpty()) {
+            _uiStateEvent.postEmptyState()
+        } else {
+            _trendsDataEvent.postValue(Result.Success(trending))
+            _uiStateEvent.postLoadedState()
+        }
+    }
+
+    private fun onError(error: Throwable) {
+        _uiStateEvent.postErrorState()
+        _trendsDataEvent.postValue(Result.Error.NetworkError(error))
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
     }
 }
