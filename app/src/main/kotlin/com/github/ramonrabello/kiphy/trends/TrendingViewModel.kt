e401904a1387ed54a234adb16bb46b39fa358dea
arctouch-ramonrabello <ramon.rabello@quizlet.com>
2019-04-10 01:44:11 -0300

    - Fix SingleLiveEvent approach - Update repository constructor to use both local and remote DS. - Update loadingTrending() method to call safeDispose().

--- app/src/main/kotlin/com/github/ramonrabello/kiphy/trends/TrendingViewModel.kt
+++ app/src/main/kotlin/com/github/ramonrabello/kiphy/trends/TrendingViewModel.kt
@@ -1,55 +1,53 @@
 package com.github.ramonrabello.kiphy.trends
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
 import com.github.ramonrabello.kiphy.BuildConfig
 import com.github.ramonrabello.kiphy.common.arch.state.Result
 import com.github.ramonrabello.kiphy.common.arch.state.SingleLiveEvent
 import com.github.ramonrabello.kiphy.common.arch.state.UiState
+import com.github.ramonrabello.kiphy.common.arch.viewmodel.BaseViewModel
+import com.github.ramonrabello.kiphy.common.data.ApiProvider
 import com.github.ramonrabello.kiphy.common.extensions.postEmptyState
 import com.github.ramonrabello.kiphy.common.extensions.postErrorState
 import com.github.ramonrabello.kiphy.common.extensions.postLoadedState
 import com.github.ramonrabello.kiphy.common.extensions.postLoadingState
-import com.github.ramonrabello.kiphy.common.data.ApiProvider
 import com.github.ramonrabello.kiphy.trends.data.TrendingDataSource
 import com.github.ramonrabello.kiphy.trends.data.TrendingRepository
 import com.github.ramonrabello.kiphy.trends.model.TrendingResponse
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
-class TrendingViewModel : ViewModel() {
+class TrendingViewModel : BaseViewModel() {
 
-    // TODO: move to a BaseViewModel
     private val _trendsDataEvent by lazy { SingleLiveEvent<Result<TrendingResponse>>() }
-    val trendsDataEvent: SingleLiveEvent<Result<TrendingResponse>>
+    val trendsDataEvent: LiveData<Result<TrendingResponse>>
         get() = _trendsDataEvent
 
-    // TODO: move to a BaseViewModel
     private val _uiStateEvent by lazy { SingleLiveEvent<UiState>() }
-    val uiStateEvent: SingleLiveEvent<UiState>
+    val uiStateEvent: LiveData<UiState>
         get() = _uiStateEvent
 
     private val repository by lazy {
         val api = ApiProvider.providesTrendingApi()
-        TrendingRepository(TrendingDataSource.Remote(api))
+        TrendingRepository(
+                TrendingDataSource.Remote(api),
+                TrendingDataSource.Local()
+        )
     }
-
-    // TODO: move to a BaseViewModel
-    private val compositeDisposable by lazy { CompositeDisposable() }
 
     fun loadTrending() {
         if (BuildConfig.GIPHY_API_KEY.isEmpty()) {
             _uiStateEvent.postErrorState()
             _trendsDataEvent.postValue(Result.Error.ApiKeyNotSetError)
         } else {
-            val disposable = repository.loadTrending()
+            repository.loadTrending()
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .doOnSubscribe { _uiStateEvent.postLoadingState() }
-                    .doOnSuccess { trending -> onTrendingLoaded(trending) }
-                    .doOnError { error -> onError(error) }
-                    .subscribe()
-            compositeDisposable.add(disposable)
+                    .subscribe(
+                            { trending -> onTrendingLoaded(trending) },
+                            { error -> onError(error) })
+                    .safeDispose()
         }
     }
 
@@ -65,10 +63,5 @@ class TrendingViewModel : ViewModel() {
     private fun onError(error: Throwable) {
         _uiStateEvent.postErrorState()
         _trendsDataEvent.postValue(Result.Error.NetworkError(error))
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        compositeDisposable.clear()
     }
 }
